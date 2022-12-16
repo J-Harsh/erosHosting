93a5847cdfd62559cf577c7bde122c5aa623b8da
harsh <harshjain172001@gmail.com>
2022-12-17 00:41:07 +0530

    mesage

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 dotenv.config();
